179a95ae8d9220182dffa4805cd91006c7543dcc
hamed <hamediran1999@gmail.com>
2021-09-11 19:33:07 +0430

    remove static url

--- core/core/urls.py
+++ core/core/urls.py
@@ -19,4 +19,4 @@ from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
